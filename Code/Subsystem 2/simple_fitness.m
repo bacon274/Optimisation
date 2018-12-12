eb19b551f119e970c2540abe54cba0e3e533546a
connieDodgshon <45629216+connieDodgshon@users.noreply.github.com>
2018-12-12 14:02:27 +0000

    Delete simple_fitness.m

--- Code/Subsystem 2/simple_fitness.m
+++
@@ -1,5 +0,0 @@
-% genetic algorithm and constraints optimisation method --> making function
-function y = simple_fitness(x)
-    y =((124.7) + (-22.91*x(1)) + (-0.03685*x(2))+ (-1.043*x(1)^2) + (-4.239*x(1)*x(2)) + (-0.4944*x(2)^2))/20689;
-    
-
